e721d1b191b5b3f0af50aeb089a045afda9d7311
Amit Sharma <34209592+amit205@users.noreply.github.com>
2018-07-01 16:05:30 +0530

    Update Titanic_submission.py

--- Titanic_submission.py
+++ Titanic_submission.py
@@ -5,14 +5,13 @@ Created on Fri Jun 29 17:17:45 2018
 @author: amit
 """
 
-# Classification template
-
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+# Importing the dataset. Importing Pclass, Sex, Age, SibSp, Parch columns for train.csv and test.csv. 
+# Also importing Survived column in variable y.
 dataset = pd.read_csv('train.csv')
 X = dataset.iloc[:, [2,4,5,6,7]].values
 y = dataset.iloc[:, 1].values
@@ -44,28 +43,32 @@ X = sc.fit_transform(X)
 X_test = sc.transform(X_test)
 
 # Fitting classifier to the Training set
-# Create your classifier here
+# Fitting Random Forest Classifier to the training set
 """from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators = 10, criterion = 'gini', random_state = 0)
 classifier.fit(X, y)"""
 
+# Fitting Logistics Regression classifier to the training set
 """from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X, y)"""
 
+# Fitting KNeighbors Classifier to the training set
 """from sklearn.neighbors import KNeighborsClassifier
 classifier = KNeighborsClassifier(n_neighbors = 100, metric = 'minkowski', p = 2, weights = 'distance')
 classifier.fit(X, y)"""
 
+# Fitting gaussian kernel SVM to the training set. It gives the best results. Hence, uncommented.
 from sklearn.svm import SVC
 classifier = SVC(kernel = 'rbf', random_state = 0)
 classifier.fit(X, y)
 
+# Fitting Naive Bayes classifier to the training set
 """from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
 classifier.fit(X, y)"""
 
-# Fitting SVM to the Training set
+# Fitting Linear SVM to the Training set
 """from sklearn.svm import SVC
 classifier = SVC(kernel = 'linear', random_state = 0)
 classifier.fit(X, y)"""
@@ -76,44 +79,3 @@ y_pred = pd.DataFrame(y_pred)
 y_pred.columns = ['Survived']
 results = pd.concat([passengerId, y_pred], axis = 1)
 results.to_csv('results.csv', index = False)
-
-
-# Making the Confusion Matrix
-"""from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
-
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()"""
